b1a949d13e2064ddda6b2d8312026176cfb5500a
Sathwik-git <sathwikbodakuntla041@gmail.com>
2024-09-27 14:12:57 +0530

    implemented signin/login using jwt

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,70 @@
+import * as dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
 const app = express();
 const prisma = new PrismaClient();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined in the environment variables");
+}
+
 app.use(express.json());
 
 app.post("/user/signup", async (req, res) => {
-  const body = req.body;
-  const hashedPassword = await bcrypt.hash(body.password, 5);
-  
+  const { username, email, password } = req.body;
+
   try {
+    const hashedPassword = await bcrypt.hash(password, 5);
     const User = await prisma.user.create({
       data: {
-        username: body.username,
-        email: body.email,
+        username: username,
+        email: email,
         password: hashedPassword,
       },
     });
     console.log(User);
-    res.status(200).json("user signed in successfully");
+    res.status(200).json("user signed up successfully");
   } catch {
     res.status(400).json("Something went wrong");
   }
 });
 
-app.post("user/login", (req, res) => {
-  //verify the data from db
-  //password should be compare
-  //return the token(jwt)
+app.post("/user/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      res.status(403).json({ msg: "User doesn't exists" });
+      return;
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      const token = jwt.sign(
+        {
+          id: user.userId,
+        },
+        JWT_SECRET
+      );
+      res.status(200).json({ token: token });
+      return;
+    } else {
+      res.status(403).json({ msg: "wrong credentials" });
+    }
+  } catch {
+    res.status(500).json({ msg: "Something went wrong" });
+  }
 });
 
 // Get all auction items
